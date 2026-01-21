80c849aa04f32397241579f126704ac3d116c208
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-21 11:18:25 -0300

    Add templates and repeat password

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,51 @@
-<h1>Crea Tu Cuenta</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php'; ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu Cuenta en UpTask</p>
+
+        <form action="/" class="formulario" method="POST">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input
+                    type="text"
+                    name="nombre"
+                    id="nombre"
+                    placeholder="Tu Nombre">
+            </div>
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Tu Email">
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Tu Password">
+            </div>
+
+            <div class="campo">
+                <label for="password2">Repetir Password</label>
+                <input
+                    type="password"
+                    name="password2"
+                    id="password2"
+                    placeholder="Repite tu Password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/">¿Ya Tienes Cuenta? Iniciar Sesión</a>
+            <a href="/olvide">¿Olvidaste tu Password?</a>
+        </div>
+    </div> <!-- .contenedor-sm -->
+</div>
