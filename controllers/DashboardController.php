e706f425e8f1d453e37b7c82d0d733962411531e
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-26 13:04:21 -0300

    Add isAuth()

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -17,13 +17,13 @@ class DashboardController {
     }
 
     public static function crear_proyecto(Router $router) {
-
         session_start();
-
         isAuth();
+        $alertas = [];
         
         $router->render('dashboard/crear-proyecto', [
-            'titulo' => 'Crear Proyecto'
+            'titulo' => 'Crear Proyecto',
+            'alertas' => $alertas
         ]);
         
     }
