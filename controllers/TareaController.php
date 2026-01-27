73d33f2552f39f790c2ed0a850a41878f6aa6845
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-27 18:05:18 -0300

    Verify if project exists

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
+
 class TareaController {
 
     public static function index() {
@@ -10,12 +12,28 @@ class TareaController {
 
     public static function crear() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            session_start();
 
-        $respuesta = [
-            'proyectoId' => $_POST['proyectoId']
-        ];
+            $proyectoId = $_POST['proyectoId'];
 
-        echo json_encode($respuesta);
+            $proyecto = Proyecto::where('url', $proyectoId);
+
+            /** @var \Model\Proyecto $proyecto  marcaba error en ->propietarioId*/
+            if (!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un Error al Agregar la Tarea'
+                ];
+                echo json_encode($respuesta);
+                
+            } else {
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'mensaje' => 'Tarea Agregada Correctamente'
+                ];
+                echo json_encode($respuesta);
+
+            }
         }
     }
         
