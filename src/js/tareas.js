4834e116470d1c079d3605b6af3a5e4483097115
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-27 11:24:03 -0300

    Add JavaScript, show form

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function() {
+    // Boton para mostrar el modal de Agregar Tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        
+    }
+})();
