321ae7f586c07c9ec85ea6912bd942f8add00c00
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-29 11:29:22 -0300

    Fetch and filter tasks based on user selection

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -2,12 +2,33 @@
 
     obtenerTareas();
     let tareas = [];
+    let filtradas = [];
 
     // Boton para mostrar el modal de Agregar Tarea
     const nuevaTareaBtn = document.querySelector('#agregar-tarea');
     nuevaTareaBtn.addEventListener('click', function() {
         mostrarFormulario();
     });
+
+    
+    // Filtros de búsqueda
+    const filtros = document.querySelectorAll('#filtros input[type="radio"]');
+    filtros.forEach( radio => {
+        radio.addEventListener('input', filtrarTareas);
+    })
+
+
+    function filtrarTareas(e) {
+        const filtro = e.target.value;
+
+        if (filtro !== '') {
+            filtradas = tareas.filter(tarea => tarea.estado === filtro);
+        } else {
+            filtradas = [];
+        }
+
+        mostrarTareas();
+    }
 
 
     async function obtenerTareas() {
@@ -29,7 +50,9 @@
     function mostrarTareas() {
         limpiarTareas();
 
-        if (tareas.length === 0) {
+        const arrayTareas = filtradas.length ? filtradas : tareas;
+
+        if (arrayTareas.length === 0) {
             const contenedorTareas = document.querySelector('#listado-tareas');
 
             const textoNoTareas = document.createElement('LI');
@@ -45,7 +68,7 @@
             1: 'Completa'
         }
 
-        tareas.forEach(tarea => {
+        arrayTareas.forEach(tarea => {
             const contenedorTarea = document.createElement('LI');
             contenedorTarea.dataset.tareaId = tarea.id;
             contenedorTarea.classList.add('tarea');
