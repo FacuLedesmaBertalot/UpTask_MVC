4c6a7ca77586e4335653939cd27754012c38fdbc
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-01-30 12:51:40 -0300

    Iniciate form  perfil

--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,4 +1,35 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
+<div class="contenedor-sm">
+    <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
+    <form action="" class="formulario" method="POST">
+
+        <div class="campo">
+            <label for="nombre">Nombre</label>
+            <input 
+                type="text" 
+                name="nombre" 
+                value="<?php echo $usuario->nombre ; ?>"
+                placeholder="Tu Nombre"
+            >
+        </div>
+
+        <div class="campo">
+            <label for="email">Email</label>
+            <input 
+                type="email" 
+                name="email" 
+                value="<?php echo $usuario->email ; ?>"
+                placeholder="Tu Email"
+            >
+        </div>
+
+
+        <input type="submit" value="Guardar Cambios">
+
+    </form>
+</div>
+
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
